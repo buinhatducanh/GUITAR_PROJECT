dd26414c482558bd887cad4083911187fb319ee5
Bùi Nhật Đức Anh <124678780+buinhatducanh@users.noreply.github.com>
2026-02-25 19:45:53 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Embedder-Policy': 'unsafe-none',
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
